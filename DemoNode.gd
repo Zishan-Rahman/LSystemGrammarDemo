9650ab337e92c7045cac712bdcc902d2899480a5
Zishan Rahman <zishanfrahman@outlook.com>
2023-02-23 20:27:34 +0000

    Convert to Godot 4 and add additional rule configurations (et cetera)

--- DemoNode.gd
+++ DemoNode.gd
@@ -1,27 +1,58 @@
 extends Node
 
-# https://youtu.be/feNVBEPXAcE?t=77
+# Basic: https://youtu.be/feNVBEPXAcE?t=77 (L = +)
+# Choices: http://paulbourke.net/fractals/lsys/
 
-const F = "F"
-const R = "R"
-const L = "L" # may be used later
+@export_enum("basic", "choices", "deterministic") var choices: String
+@export var axiom: String
+@onready var string: String
+@onready var line = $Line2D
+@onready var label = $TextLabel
+@onready var rules: Array[Dictionary]
 
-export (String) var axiom
-onready var string: String
+var sample_rule: Dictionary = {
+	"from": "F",
+	"to": "F+F"
+}
 
-var rules = [
-	{
-		"from": F,
-		"to": (F + R + F)
-	}
-]
+func set_values():
+	if choices == "basic":
+		rules = [
+			{
+				"from": "F",
+				"to": "F+F"
+			}
+		]
+		axiom = "F+"
+	elif choices == "choices":
+		rules = [
+			{
+				"from": "F",
+				"to": "F+F−F−FF+F+F−F"
+			}
+		]
+		axiom = "F+F+F+F"
+	elif choices == "deterministic":
+		rules = [
+			{
+				"from": "a",
+				"to": "ab"
+			},
+			{
+				"from": "b",
+				"to": "a"
+			}
+		]
+		axiom = "b"
 
 func _ready():
+	set_values()
 	string = axiom
-	print(axiom)
+	label.size.x = get_viewport().size.x
+	label.text = string
 	$Timer.start()
 
 func _on_Timer_timeout():
 	for rule in rules:
 		string = string.replace(rule["from"], rule["to"])
-	print(string)
+	label.text = string
